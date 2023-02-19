ed7760cc94310ebb3158848b4e618e75b242fd1b
Tornike Zhizhiashvili <t.zhizhiashvili@adjarabet.com>
2023-02-19 18:53:00 +0400

    add hamburger

--- src/BaseLayout/Header.js
+++ src/BaseLayout/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Header.scss";
 
 import logo from "../assets/imgs/logo1.svg"
@@ -6,13 +6,17 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     
+const [menuToggler, setMenuToggler] = useState(false)
+
+
+    
     
     return (
         <div className='header'>
             <Link to="/" class="header_logo">
                 <img src={logo} alt=""/>
             </Link>
-            <nav>
+            <nav className='nav'>
                 <ul>
                     <li>
                         <NavLink to="/">Home</NavLink>
@@ -40,6 +44,38 @@ const Header = () => {
                     </li>
                 </ul>
             </nav>
+
+            <div className="hamburger-icon" id="icon" onClick={()=>setMenuToggler(!menuToggler)}>
+                <div className={`icon-1` + (menuToggler ? " a" : "")} id="a"></div>
+                <div className={`icon-2` + (menuToggler ? " c" : "")} id="b"></div>
+                <div className={`icon-3` + (menuToggler ? " b" : "")} id="c"></div>
+                <div className="clear"></div>
+            </div>
+            
+            <nav id="nav"  className={`hamburger_nav` + (menuToggler ? " show" : "")}>
+                
+                <ul>
+                <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                </ul>
+            
+            </nav>
+  
+            <div className={`dark-blue` + (menuToggler ? " slide" : "")} id="blue"></div>
+
         </div>
     );
 };
--- src/Components/HomeComponents.js/HomeMain.js
+++ src/Components/HomeComponents.js/HomeMain.js
@@ -5,7 +5,21 @@ import "./HomeMain.scss";
 const HomeMain = () => {
     return (
         <div className='home_main'>
-            
+            <div className='main_banner'>
+
+                <div class="content">
+                    <h2>Change Your Life !!!</h2>
+                    <h2>Change Your Life !!!</h2>
+	            </div>
+                <div className='banner_bottom_text'>
+                    <span>always be</span>
+                    <div class="message">
+                        <div class="word1">Healthy</div>
+                        <div class="word2">Happy</div>
+                        <div class="word3">Strong</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
