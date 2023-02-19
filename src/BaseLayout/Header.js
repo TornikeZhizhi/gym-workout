0b75b9fdd2e96228f1bb889c773172a859fecaab
Tornike Zhizhiashvili <t.zhizhiashvili@adjarabet.com>
2023-02-20 00:43:59 +0400

    add navigation

--- src/BaseLayout/Header.js
+++ src/BaseLayout/Header.js
@@ -12,71 +12,77 @@ const [menuToggler, setMenuToggler] = useState(false)
     
     
     return (
-        <div className='header'>
-            <Link to="/" class="header_logo">
-                <img src={logo} alt=""/>
-            </Link>
-            <nav className='nav'>
-                <ul>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                </ul>
-            </nav>
+            <>
+           
+            <div className='header'>
+                <Link to="/" class="header_logo">
+                    <img src={logo} alt=""/>
+                </Link>
+                <nav className='navbar'>
+                    <ul>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                    </ul>
+                </nav>
 
-            <div className="hamburger-icon" id="icon" onClick={()=>setMenuToggler(!menuToggler)}>
-                <div className={`icon-1` + (menuToggler ? " a" : "")} id="a"></div>
-                <div className={`icon-2` + (menuToggler ? " c" : "")} id="b"></div>
-                <div className={`icon-3` + (menuToggler ? " b" : "")} id="c"></div>
-                <div className="clear"></div>
+                <div className="hamburger-icon" id="icon" onClick={()=>setMenuToggler(!menuToggler)}>
+                    <div className={`icon-1` + (menuToggler ? " a" : "")} id="a"></div>
+                    <div className={`icon-2` + (menuToggler ? " c" : "")} id="b"></div>
+                    <div className={`icon-3` + (menuToggler ? " b" : "")} id="c"></div>
+                    <div className="clear"></div>
+                </div>
             </div>
-            
-            <nav id="nav"  className={`hamburger_nav` + (menuToggler ? " show" : "")}>
-                
-                <ul>
-                <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                </ul>
-            
-            </nav>
-  
-            <div className={`dark-blue` + (menuToggler ? " slide" : "")} id="blue"></div>
 
-        </div>
+            <div class={`nav`+ (menuToggler ? " nav-active" : "")}>
+                <div class="nav__content">
+                    <div className='nav_content_left'>
+                    <nav className='navbar_x'>
+                        <ul>
+                            <li className='nav__list-item'>
+                                <NavLink to="/">Home</NavLink>
+                            </li>
+                            <li className='nav__list-item'>
+                                <NavLink to="/">About</NavLink>
+                            </li>
+                            <li className='nav__list-item'>
+                                <NavLink to="/">Prices</NavLink>
+                            </li>
+                            <li className='nav__list-item'>
+                                <NavLink to="/">Trainers</NavLink>
+                            </li>
+                            <li className='nav__list-item'>
+                                <NavLink to="/">Contact Us</NavLink>
+                            </li>
+                        </ul>
+                     </nav>
+                    </div>
+                    <div className='nav_content_right'></div>
+                    
+                </div>
+            </div>
+            </>
     );
 };
 
